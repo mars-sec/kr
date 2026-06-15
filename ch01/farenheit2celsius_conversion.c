f2436c6eae261a5205e394b62bd9b789a7299984
Marshall Yanis <marshall.e.yanis@gmail.com>
2026-06-15 01:37:27 +0000

    Created farenheit to celcius conversion

--- ch01/farenheit2celsius_conversion.c
+++ ch01/farenheit2celsius_conversion.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	
+	float far, cel;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	far = lower;
+	while (far <= upper) {
+		cel = (5.0/9.0) * (far-32.0);
+		printf("%3.0f %6.1f\n", far, cel);
+		far = far + step;
+	}
 }
