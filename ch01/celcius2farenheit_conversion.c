592111acb56bd0d50c0fcd331f1864d1a1eb547b
Marshall Yanis <marshall.e.yanis@gmail.com>
2026-06-16 01:21:40 +0000

    Exercise 1-4: Created corresponding C to F conversion program.

---
+++ ch01/celcius2farenheit_conversion.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void print_dashes(int n) {
+	int i;
+	for (i = 0; i< n; i++) {
+		putchar('-');
+	}
+}
+
+void print_header(const char *col1, const char *col2, int w1, int w2) {
+	printf("%*s %*s\n", w1, col1, w2, col2);
+	print_dashes(w1);
+	putchar(' ');
+	print_dashes(w2);
+	putchar('\n');
+
+}
+
+
+int main() {
+	float far, cel;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	print_header("C", "F", 3, 6);
+
+	cel = lower;
+	while (cel <= upper) {
+		far = (cel * (9.0/5.0)) + 32;
+		printf("%3.0f %6.1f\n", cel, far);
+		cel = cel + step;
+	}
+}
